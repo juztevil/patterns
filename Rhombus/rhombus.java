4b6c723e907c698b66954fd4689e3ac6953b653c
juztevil <121966707+juztevil@users.noreply.github.com>
2024-02-17 22:45:36 +0530

    Create rhombus.java

---
+++ Rhombus/rhombus.java
@@ -0,0 +1,45 @@
+import java.util.*;
+class rhombus{
+	public static void main(String args[]){
+		Scanner scan=new Scanner(System.in);
+		printTheRhombus(scan.nextInt());
+	}
+	public static void printTheRhombus(int n){
+		for(int i=0;i<n;++i){
+			for(int j=0;j<i;++j)
+			System.out.print(" ");
+			for(int k=0;k<n;++k)
+			System.out.print("*");
+			System.out.println();
+		}
+	}
+}
+
+
+/*
+
+Output
+patterns>javac rhombus.java
+
+patterns>java rhombus
+5
+*****
+ *****
+  *****
+   *****
+    *****
+
+patterns>java rhombus
+10
+**********
+ **********
+  **********
+   **********
+    **********
+     **********
+      **********
+       **********
+        **********
+         **********
+
+*/
