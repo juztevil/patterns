3ce5804bd42353ebd313af1a87bf5c40083d2f42
juztevil <121966707+juztevil@users.noreply.github.com>
2024-02-17 22:42:02 +0530

    Create invertedLeftHalfPyramid.java

---
+++ Inverted Left Half Pyramid/invertedLeftHalfPyramid.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class invertedLeftHalfPyramid{
+	public static void main(String args[]){
+		Scanner scan=new Scanner(System.in);
+		printThePyramid(scan.nextInt());
+	}
+	public static void printThePyramid(int n){
+		for(int i=0;i<n;++i){
+			for(int j=0;j<i;++j)
+			System.out.print(" ");
+			for(int k=n-i;k>0;--k)
+			System.out.print("*");
+			System.out.println();
+		}
+	}
+}
+
+/*
+
+patterns>java invertedLeftHalfPyramid
+5
+*****
+ ****
+  ***
+   **
+    *
+
+patterns>java invertedLeftHalfPyramid
+10
+**********
+ *********
+  ********
+   *******
+    ******
+     *****
+      ****
+       ***
+        **
+         *
+
+*/
