e4003b2e28435653e68c537c5823e5a023448dea
juztevil <121966707+juztevil@users.noreply.github.com>
2024-02-19 19:07:42 +0530

    Create numberTriangular.java

---
+++ Number Triangular/numberTriangular.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class numberTriangular{
+	public static void main(String args[]){
+		Scanner scan=new Scanner(System.in);
+		printTheNumberTriangular(scan.nextInt());
+	}
+	public static void printTheNumberTriangular(int n){
+		for(int i=0;i<n;++i){
+			for(int j=0;j<n-i-1;++j)
+			System.out.print(" ");
+			for(int k=0;k<=i;++k)
+			System.out.print((i+1)+" ");
+			System.out.println();
+		}
+	}
+}
+
+/*
+
+Output
+patterns>javac numberTriangular.java
+
+patterns>java numberTriangular
+5
+    1
+   2 2
+  3 3 3
+ 4 4 4 4
+5 5 5 5 5
+
+patterns>java numberTriangular
+10
+         1
+        2 2
+       3 3 3
+      4 4 4 4
+     5 5 5 5 5
+    6 6 6 6 6 6
+   7 7 7 7 7 7 7
+  8 8 8 8 8 8 8 8
+ 9 9 9 9 9 9 9 9 9
+10 10 10 10 10 10 10 10 10 10
+
+patterns>
+
+*/
