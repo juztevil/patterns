ba26d2ceeafda4c280088f2c603ebf8576ac0765
juztevil <121966707+juztevil@users.noreply.github.com>
2024-02-19 11:41:44 +0530

    Create hollowFullPyramid.java

---
+++ Hollow Full Pyramid/hollowFullPyramid.java
@@ -0,0 +1,59 @@
+import java.util.*;
+class hollowFullPyramid{
+	public static void main(String args[]){
+		Scanner scan=new Scanner(System.in);
+		printTheHollowPyramid(scan.nextInt());
+	}
+	public static void printTheHollowPyramid(int n){
+		int stars=1;
+		for(int i=n;i>0;--i){
+			for(int j=i-1;j>0;--j)
+			System.out.print(" ");
+			if(i==n||i==1){
+				for(int temp=0;temp<stars;++temp)
+				System.out.print("*");
+				System.out.println();
+				stars+=2;
+				continue;
+			}
+			for(int k=0;k<stars;++k){
+				if(k==0||k==stars-1){
+					System.out.print("*");
+					continue;
+				}
+				System.out.print(" ");
+			}
+			System.out.println();
+			stars+=2;
+		}
+	}
+}
+
+/*
+
+Output
+patterns>javac hollowFullPyramid.java
+
+patterns>java hollowFullPyramid
+5
+    *
+   * *
+  *   *
+ *     *
+*********
+
+patterns>java hollowFullPyramid
+10
+         *
+        * *
+       *   *
+      *     *
+     *       *
+    *         *
+   *           *
+  *             *
+ *               *
+*******************
+
+
+*/
