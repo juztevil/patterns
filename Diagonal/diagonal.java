ac025f9244cdb5349a731d5535bd005b135b4595
Harikrishna T <harikrishna09876543@gmail.com>
2024-03-04 21:55:54 +0530

    committing

--- Diagonal/diagonal.java
+++ Diagonal/diagonal.java
@@ -1,6 +1,74 @@
 import java.util.*;
 class diagonal{
     public static void main(String args[]){
-
+        Scanner scan=new Scanner(System.in);
+        printTheDiagonal(scan.nextInt());
+        scan.close();
+    }
+    public static void printTheDiagonal(int n){
+        for(int i=0;i<n;++i){
+            for(int j=0;j<n-i-1;++j)
+            System.out.print(" ");
+            System.out.println("*");
+        }
     }
 }
+
+/*
+    Output
+    patterns>javac diagonal.java
+
+patterns>java diagonal
+1
+*
+
+patterns>java diagonal
+4
+   *
+  *
+ *
+*
+
+patterns>java diagonal
+5
+    *
+   *
+  *
+ *
+*
+
+patterns>java diagonal
+9
+        *
+       *
+      *
+     *
+    *
+   *
+  *
+ *
+*
+
+patterns>java diagonal
+20
+                   *
+                  *
+                 *
+                *
+               *
+              *
+             *
+            *
+           *
+          *
+         *
+        *
+       *
+      *
+     *
+    *
+   *
+  *
+ *
+*
+*/
